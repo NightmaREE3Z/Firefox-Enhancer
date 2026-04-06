e2a4669d30bffbc5ac5516a33d34e11cda42f41b
NightmaREE3Z <XanaronnoSucks@gmail.com>
2026-04-06 04:06:43 +0300

    22.6.7

--- background.js
+++ background.js
@@ -189,11 +189,11 @@ const urlsToBlock = [
 
 // Blocked TLDs for auto-closure
 const blockedTLDs = [
-    '.ai', '.art', '.io', '.makeup', '.off', '.club', '.id', '.it', '.best', '.cc', '.cn', '.click',
+    '.ai', '.art', '.makeup', '.off', '.club', '.id', '.it', '.best', '.cc', '.cn', '.click', '.exposed',
     '.you', '.to', '.top', '.me', '.us', '.ru', '.vip', '.online', '.hot', '.her', '.sex', '.xxx', '.nsfw',
     '.porn', '.show', '.work', '.fit', '.tool', '.tools', '.system', '.systems', '.surf', '.review', '.asia',
-    '.tokyo', '.monster', '.info', '.机构', '.xn--nqv7f', '.one', '.ee', '.in', '.gf', '.fox', '.fun', '.exposed',
-    '.fyi', '.fr', '.life', '.now', '.today', '.world', '.xyz', '.zone', '.nude'
+    '.tokyo', '.monster', '.info', '.机构', '.xn--nqv7f', '.one', '.ee', '.in', '.gf', '.fox', '.fun', '.fyi', 
+    '.fr', '.life', '.now', '.today', '.world', '.xyz', '.zone', '.nude'
 ];
 
 // Dynamically convert TLDs into Match Patterns and inject into urlsToBlock
